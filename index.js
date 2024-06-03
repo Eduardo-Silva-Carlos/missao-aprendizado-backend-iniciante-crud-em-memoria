8af45e0c635c8fbd336c79c631ae3787e936a054
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-06-03 16:10:37 +0000

    EndPoint 'Read All' [GET] /personagem ' implementado'

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+const lista =  ['Java','Kotlin','Android']
+
+// EndPoint Read All 'Criando uma lista'
+
+app.get ('/personagem', function (req, res) {
+  res.send(lista)
+})
+
+
 app.listen(3000)
+
