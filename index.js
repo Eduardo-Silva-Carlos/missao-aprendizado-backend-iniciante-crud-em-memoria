d3639bb9061679894cc87b487e65f5750eef2a03
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-06-03 17:59:39 +0000

    EndPoint Read By id  item '/personagem/:id' criado

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-const lista =  ['Java','Kotlin','Android']
+const lista = ['Java', 'Kotlin', 'Android']
 
-// EndPoint Read All 'Criando uma lista'
+  // EndPoint Read All '[GET] /personagem'
 
-app.get ('/personagem', function (req, res) {
+app.get('/personagem', function (req, res) {
   res.send(lista)
 })
 
+  // EndPoint Read BY ID [GET]/personagem/:id
+app.get('/personagem/:id', function (req, res) {
+  // Acessando os parametros de rota ID
+  const id = req.params.id
+
+  // Acessa o item na lista com id - 1
+  const item = lista[id - 1]
+  //Enviamos o item como resposta 
+
+  res.send(item)
+})
 
 app.listen(3000)
 
